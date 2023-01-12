60987436efd3d5d014495da71ec8cab7271339b3
Pavel <pavelatavin@yandex.ru>
2023-01-12 15:07:52 +0500

    initial commit

--- strings.py
+++ strings.py
@@ -1,16 +1,16 @@
 BOT_TOKEN_FILENAME = 'token.txt'
 
-# символы, которые используются
+
 SYMBOL_X = '❌'
 SYMBOL_O = '⭕'
 SYMBOL_UNDEF = '◻'
 
-# ответы бота
+
 ANSW_YOUR_TURN = 'Ваш ход'
 ANSW_YOU_WIN = 'Вы победили'
 ANSW_BOT_WIN = 'Победил бот'
 ANSW_DRAW = 'Ничья'
-ANSW_HELP = 'Бот для игры в крестики-нолики\nСоздатель: @pashabezk\n\n' \
+ANSW_HELP = 'Бот для игры в крестики-нолики\n' \
             'Используйте команду /new_game для начала новой игры'
 
 # ошибки
